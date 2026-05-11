180ad1bb11bee8c3fbe0f4540e21c8a14505e558
nsfisis <nsfisis@gmail.com>
2026-05-12 02:57:34 +0900

    feat(port): port LockArrayRepository.php

--- crates/shirabe/src/repository/lock_array_repository.rs
+++ crates/shirabe/src/repository/lock_array_repository.rs
@@ -1,1 +1,17 @@
 //! ref: composer/src/Composer/Repository/LockArrayRepository.php
+
+use crate::repository::array_repository::ArrayRepository;
+use crate::repository::canonical_packages_trait::CanonicalPackagesTrait;
+
+#[derive(Debug)]
+pub struct LockArrayRepository {
+    inner: ArrayRepository,
+}
+
+impl CanonicalPackagesTrait for LockArrayRepository {}
+
+impl LockArrayRepository {
+    pub fn get_repo_name(&self) -> &str {
+        "lock repo"
+    }
+}
