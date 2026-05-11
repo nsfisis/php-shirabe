4211106824cd8c1beab56d2bfd0e52288ed07361
nsfisis <nsfisis@gmail.com>
2026-05-12 03:35:28 +0900

    feat(port): port Capable.php

--- crates/shirabe/src/plugin/capable.rs
+++ crates/shirabe/src/plugin/capable.rs
@@ -1,1 +1,8 @@
 //! ref: composer/src/Composer/Plugin/Capable.php
+
+use indexmap::IndexMap;
+
+// TODO(plugin): Plugin API - interface for plugins that expose capability implementations
+pub trait Capable {
+    fn get_capabilities(&self) -> IndexMap<String, String>;
+}
