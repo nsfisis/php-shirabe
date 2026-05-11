3f2f6f2c3552f014e840c58a7adf61b239b675a0
nsfisis <nsfisis@gmail.com>
2026-05-12 03:36:32 +0900

    feat(port): port ArchiverInterface.php

--- crates/shirabe/src/package/archiver/archiver_interface.rs
+++ crates/shirabe/src/package/archiver/archiver_interface.rs
@@ -1,1 +1,14 @@
 //! ref: composer/src/Composer/Package/Archiver/ArchiverInterface.php
+
+pub trait ArchiverInterface {
+    fn archive(
+        &self,
+        sources: String,
+        target: String,
+        format: String,
+        excludes: Vec<String>,
+        ignore_filters: bool,
+    ) -> String;
+
+    fn supports(&self, format: String, source_type: Option<String>) -> bool;
+}
