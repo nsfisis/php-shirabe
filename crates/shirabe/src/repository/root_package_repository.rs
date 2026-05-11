2161c564c7e627d44f4eb98f64b7a577f6623722
nsfisis <nsfisis@gmail.com>
2026-05-12 03:17:17 +0900

    feat(port): port RootPackageRepository.php

--- crates/shirabe/src/repository/root_package_repository.rs
+++ crates/shirabe/src/repository/root_package_repository.rs
@@ -1,1 +1,21 @@
 //! ref: composer/src/Composer/Repository/RootPackageRepository.php
+
+use crate::package::root_package_interface::RootPackageInterface;
+use crate::repository::array_repository::ArrayRepository;
+
+#[derive(Debug)]
+pub struct RootPackageRepository {
+    inner: ArrayRepository,
+}
+
+impl RootPackageRepository {
+    pub fn new(package: Box<dyn RootPackageInterface>) -> Self {
+        Self {
+            inner: ArrayRepository::new(vec![package]),
+        }
+    }
+
+    pub fn get_repo_name(&self) -> String {
+        "root package repo".to_string()
+    }
+}
