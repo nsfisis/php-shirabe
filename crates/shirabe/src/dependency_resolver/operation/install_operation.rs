35a96b4fbf25c6463c00f31f1a295e77f6fcb5d4
nsfisis <nsfisis@gmail.com>
2026-05-12 04:04:47 +0900

    feat(port): port InstallOperation.php

--- crates/shirabe/src/dependency_resolver/operation/install_operation.rs
+++ crates/shirabe/src/dependency_resolver/operation/install_operation.rs
@@ -1,1 +1,47 @@
 //! ref: composer/src/Composer/DependencyResolver/Operation/InstallOperation.php
+
+use crate::dependency_resolver::operation::operation_interface::OperationInterface;
+use crate::dependency_resolver::operation::solver_operation::SolverOperation;
+use crate::package::package_interface::PackageInterface;
+
+#[derive(Debug)]
+pub struct InstallOperation {
+    pub(crate) package: Box<dyn PackageInterface>,
+}
+
+impl InstallOperation {
+    pub fn new(package: Box<dyn PackageInterface>) -> Self {
+        Self { package }
+    }
+
+    pub fn get_package(&self) -> &dyn PackageInterface {
+        self.package.as_ref()
+    }
+
+    pub fn format(package: &dyn PackageInterface, lock: bool) -> String {
+        format!(
+            "{}<info>{}</info> (<comment>{}</comment>)",
+            if lock { "Locking " } else { "Installing " },
+            package.get_pretty_name(),
+            package.get_full_pretty_version(),
+        )
+    }
+}
+
+impl SolverOperation for InstallOperation {
+    const TYPE: &'static str = "install";
+}
+
+impl OperationInterface for InstallOperation {
+    fn get_operation_type(&self) -> String {
+        Self::TYPE.to_string()
+    }
+
+    fn show(&self, lock: bool) -> String {
+        Self::format(self.package.as_ref(), lock)
+    }
+
+    fn to_string(&self) -> String {
+        self.show(true)
+    }
+}
