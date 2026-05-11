7b7afb121df73769821e777145a0889d0bf2d2d2
nsfisis <nsfisis@gmail.com>
2026-05-12 03:35:02 +0900

    feat(port): port JsonValidationException.php

--- crates/shirabe/src/json/json_validation_exception.rs
+++ crates/shirabe/src/json/json_validation_exception.rs
@@ -1,1 +1,22 @@
 //! ref: composer/src/Composer/Json/JsonValidationException.php
+
+use shirabe_php_shim::Exception;
+
+#[derive(Debug)]
+pub struct JsonValidationException {
+    inner: Exception,
+    pub(crate) errors: Vec<String>,
+}
+
+impl JsonValidationException {
+    pub fn new(message: String, errors: Vec<String>) -> Self {
+        Self {
+            inner: Exception { message, code: 0 },
+            errors,
+        }
+    }
+
+    pub fn get_errors(&self) -> &Vec<String> {
+        &self.errors
+    }
+}
