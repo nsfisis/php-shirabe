598ecbb3900dc08d571a1bdccab0b9cafdc34c08
nsfisis <nsfisis@gmail.com>
2026-05-12 02:39:50 +0900

    feat(port): port FilesystemException.php

--- crates/shirabe/src/downloader/filesystem_exception.rs
+++ crates/shirabe/src/downloader/filesystem_exception.rs
@@ -1,1 +1,15 @@
 //! ref: composer/src/Composer/Downloader/FilesystemException.php
+
+use shirabe_php_shim::Exception;
+
+#[derive(Debug)]
+pub struct FilesystemException(pub Exception);
+
+impl FilesystemException {
+    pub fn new(message: String, code: i64) -> Self {
+        FilesystemException(Exception {
+            message: format!("Filesystem exception: \n{}", message),
+            code,
+        })
+    }
+}
