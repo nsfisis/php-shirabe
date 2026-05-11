e249c95c28c72317899dd0ead3105f757337ea06
nsfisis <nsfisis@gmail.com>
2026-05-12 03:06:33 +0900

    feat(port): port IgnoreNothingPlatformRequirementFilter.php

--- crates/shirabe/src/filter/platform_requirement_filter/ignore_nothing_platform_requirement_filter.rs
+++ crates/shirabe/src/filter/platform_requirement_filter/ignore_nothing_platform_requirement_filter.rs
@@ -1,1 +1,16 @@
 //! ref: composer/src/Composer/Filter/PlatformRequirementFilter/IgnoreNothingPlatformRequirementFilter.php
+
+use super::platform_requirement_filter_interface::PlatformRequirementFilterInterface;
+
+#[derive(Debug)]
+pub struct IgnoreNothingPlatformRequirementFilter;
+
+impl PlatformRequirementFilterInterface for IgnoreNothingPlatformRequirementFilter {
+    fn is_ignored(&self, _req: &str) -> bool {
+        false
+    }
+
+    fn is_upper_bound_ignored(&self, _req: &str) -> bool {
+        false
+    }
+}
