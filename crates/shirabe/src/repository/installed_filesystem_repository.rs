c812babb8dec19b2860678022cc0e8a702324b28
nsfisis <nsfisis@gmail.com>
2026-05-12 03:16:54 +0900

    feat(port): port InstalledFilesystemRepository.php

--- crates/shirabe/src/repository/installed_filesystem_repository.rs
+++ crates/shirabe/src/repository/installed_filesystem_repository.rs
@@ -1,1 +1,25 @@
 //! ref: composer/src/Composer/Repository/InstalledFilesystemRepository.php
+
+use crate::repository::filesystem_repository::FilesystemRepository;
+use crate::repository::installed_repository_interface::InstalledRepositoryInterface;
+
+#[derive(Debug)]
+pub struct InstalledFilesystemRepository {
+    inner: FilesystemRepository,
+}
+
+impl InstalledFilesystemRepository {
+    pub fn get_repo_name(&self) -> String {
+        format!("installed {}", self.inner.get_repo_name())
+    }
+}
+
+impl InstalledRepositoryInterface for InstalledFilesystemRepository {
+    fn get_dev_mode(&self) -> Option<bool> {
+        self.inner.get_dev_mode()
+    }
+
+    fn is_fresh(&self) -> bool {
+        !self.inner.file.exists()
+    }
+}
