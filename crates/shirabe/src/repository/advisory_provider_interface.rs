6a804d9206164a10a583a8b097d531af9984631a
nsfisis <nsfisis@gmail.com>
2026-05-12 03:15:58 +0900

    feat(port): port AdvisoryProviderInterface.php

--- crates/shirabe/src/repository/advisory_provider_interface.rs
+++ crates/shirabe/src/repository/advisory_provider_interface.rs
@@ -1,1 +1,24 @@
 //! ref: composer/src/Composer/Repository/AdvisoryProviderInterface.php
+
+use indexmap::IndexMap;
+use shirabe_semver::constraint::constraint_interface::ConstraintInterface;
+use crate::advisory::partial_security_advisory::PartialSecurityAdvisory;
+use crate::advisory::security_advisory::SecurityAdvisory;
+
+#[derive(Debug)]
+pub enum PartialOrSecurityAdvisory {
+    Partial(PartialSecurityAdvisory),
+    Full(SecurityAdvisory),
+}
+
+#[derive(Debug)]
+pub struct SecurityAdvisoryResult {
+    pub names_found: Vec<String>,
+    pub advisories: IndexMap<String, Vec<PartialOrSecurityAdvisory>>,
+}
+
+pub trait AdvisoryProviderInterface {
+    fn has_security_advisories(&self) -> bool;
+
+    fn get_security_advisories(&self, package_constraint_map: IndexMap<String, Box<dyn ConstraintInterface>>, allow_partial_advisories: bool) -> SecurityAdvisoryResult;
+}
