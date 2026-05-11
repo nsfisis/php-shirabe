5df72ff9fdc392529884647deac55dfbd9810476
nsfisis <nsfisis@gmail.com>
2026-05-12 02:08:30 +0900

    feat(port): port MaxFileSizeExceededException.php

--- crates/shirabe/src/downloader/max_file_size_exceeded_exception.rs
+++ crates/shirabe/src/downloader/max_file_size_exceeded_exception.rs
@@ -1,1 +1,5 @@
 //! ref: composer/src/Composer/Downloader/MaxFileSizeExceededException.php
+
+use crate::downloader::transport_exception::TransportException;
+
+pub struct MaxFileSizeExceededException(pub TransportException);
