d378ca71ecbda7a25ea4b86de0c56198536857e4
nsfisis <nsfisis@gmail.com>
2026-05-12 03:11:43 +0900

    feat(port): port CommandProvider.php

--- crates/shirabe/src/plugin/capability/command_provider.rs
+++ crates/shirabe/src/plugin/capability/command_provider.rs
@@ -1,1 +1,9 @@
 //! ref: composer/src/Composer/Plugin/Capability/CommandProvider.php
+
+// TODO(plugin): Commands Provider Interface. Plugins implementing this capability provide a list of commands.
+use crate::command::base_command::BaseCommand;
+use crate::plugin::capability::capability::Capability;
+
+pub trait CommandProvider: Capability {
+    fn get_commands(&self) -> Vec<Box<BaseCommand>>;
+}
