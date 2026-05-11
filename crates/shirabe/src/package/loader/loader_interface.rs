9822e9e0cbdb759a099ae03e2a3fa230678c6d17
nsfisis <nsfisis@gmail.com>
2026-05-12 03:27:29 +0900

    feat(port): port LoaderInterface.php

--- crates/shirabe/src/package/loader/loader_interface.rs
+++ crates/shirabe/src/package/loader/loader_interface.rs
@@ -1,1 +1,9 @@
 //! ref: composer/src/Composer/Package/Loader/LoaderInterface.php
+
+use indexmap::IndexMap;
+use shirabe_php_shim::PhpMixed;
+use crate::package::base_package::BasePackage;
+
+pub trait LoaderInterface {
+    fn load(&self, config: IndexMap<String, PhpMixed>, class: Option<String>) -> Box<BasePackage>;
+}
