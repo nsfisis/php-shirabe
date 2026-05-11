8bf6f8e8ce56ab3f5c6cf1b0fe09fe51eaae9170
nsfisis <nsfisis@gmail.com>
2026-05-12 02:40:29 +0900

    feat(port): port InstallerEvents.php

--- crates/shirabe/src/installer/installer_events.rs
+++ crates/shirabe/src/installer/installer_events.rs
@@ -1,1 +1,7 @@
 //! ref: composer/src/Composer/Installer/InstallerEvents.php
+
+pub struct InstallerEvents;
+
+impl InstallerEvents {
+    pub const PRE_OPERATIONS_EXEC: &'static str = "pre-operations-exec";
+}
