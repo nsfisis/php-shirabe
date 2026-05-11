a466a436275950ffc17b163a6a620ff78d0fcc17
nsfisis <nsfisis@gmail.com>
2026-05-12 03:03:35 +0900

    feat(port): port ComposerExcludeFilter.php

--- crates/shirabe/src/package/archiver/composer_exclude_filter.rs
+++ crates/shirabe/src/package/archiver/composer_exclude_filter.rs
@@ -1,1 +1,16 @@
 //! ref: composer/src/Composer/Package/Archiver/ComposerExcludeFilter.php
+
+use super::base_exclude_filter::BaseExcludeFilter;
+
+#[derive(Debug)]
+pub struct ComposerExcludeFilter {
+    inner: BaseExcludeFilter,
+}
+
+impl ComposerExcludeFilter {
+    pub fn new(source_path: String, exclude_rules: Vec<String>) -> Self {
+        let mut inner = BaseExcludeFilter::new(source_path);
+        inner.exclude_patterns = inner.generate_patterns(exclude_rules);
+        Self { inner }
+    }
+}
