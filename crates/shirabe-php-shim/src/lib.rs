76fa34cb4df0ac1e619ac7c5899d37b77e4e4a13
nsfisis <nsfisis@gmail.com>
2026-05-12 02:10:57 +0900

    feat(port): port IrrecoverableDownloadException.php

--- crates/shirabe-php-shim/src/lib.rs
+++ crates/shirabe-php-shim/src/lib.rs
@@ -1,4 +1,10 @@
 #[derive(Debug)]
+pub struct RuntimeException {
+    pub message: String,
+    pub code: i64,
+}
+
+#[derive(Debug)]
 pub struct UnexpectedValueException {
     pub message: String,
     pub code: i64,
--- crates/shirabe/src/exception/irrecoverable_download_exception.rs
+++ crates/shirabe/src/exception/irrecoverable_download_exception.rs
@@ -1,1 +1,5 @@
 //! ref: composer/src/Composer/Exception/IrrecoverableDownloadException.php
+
+use shirabe_php_shim::RuntimeException;
+
+pub struct IrrecoverableDownloadException(pub RuntimeException);
