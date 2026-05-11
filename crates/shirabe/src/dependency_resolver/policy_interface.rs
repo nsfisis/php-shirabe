14b75a9a50a886b895e2d1a0805c0988f2e2c7b7
nsfisis <nsfisis@gmail.com>
2026-05-12 03:10:52 +0900

    feat(port): port PolicyInterface.php

--- crates/shirabe/src/dependency_resolver/policy_interface.rs
+++ crates/shirabe/src/dependency_resolver/policy_interface.rs
@@ -1,1 +1,10 @@
 //! ref: composer/src/Composer/DependencyResolver/PolicyInterface.php
+
+use crate::dependency_resolver::pool::Pool;
+use crate::package::package_interface::PackageInterface;
+
+pub trait PolicyInterface {
+    fn version_compare(&self, a: &dyn PackageInterface, b: &dyn PackageInterface, operator: &str) -> bool;
+
+    fn select_preferred_packages(&self, pool: &Pool, literals: Vec<i64>, required_package: Option<String>) -> Vec<i64>;
+}
