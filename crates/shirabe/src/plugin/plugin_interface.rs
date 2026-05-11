fce5d59aeb5a1fdc07a0fa4b74f38ded23d9551e
nsfisis <nsfisis@gmail.com>
2026-05-12 04:14:55 +0900

    feat(port): port PluginInterface.php

--- crates/shirabe/src/plugin/plugin_interface.rs
+++ crates/shirabe/src/plugin/plugin_interface.rs
@@ -1,1 +1,14 @@
 //! ref: composer/src/Composer/Plugin/PluginInterface.php
+
+use crate::composer::Composer;
+use crate::io::io_interface::IOInterface;
+
+pub trait PluginInterface {
+    const PLUGIN_API_VERSION: &'static str = "2.9.0";
+
+    fn activate(&mut self, composer: &Composer, io: &dyn IOInterface);
+
+    fn deactivate(&mut self, composer: &Composer, io: &dyn IOInterface);
+
+    fn uninstall(&mut self, composer: &Composer, io: &dyn IOInterface);
+}
