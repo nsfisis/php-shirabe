825330d83d86048816b6e09bdc444b224a8809a0
nsfisis <nsfisis@gmail.com>
2026-05-12 02:39:17 +0900

    feat(port): port Capability.php

--- crates/shirabe/src/plugin/capability/capability.rs
+++ crates/shirabe/src/plugin/capability/capability.rs
@@ -1,1 +1,4 @@
 //! ref: composer/src/Composer/Plugin/Capability/Capability.php
+
+// TODO(plugin): Marker interface for Plugin capabilities. Every new Capability which is added to the Plugin API must implement this interface.
+pub trait Capability {}
