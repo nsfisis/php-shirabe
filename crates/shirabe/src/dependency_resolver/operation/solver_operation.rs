9a9582e9476eb73fb51413d290cbfccbc7b36f25
nsfisis <nsfisis@gmail.com>
2026-05-12 03:29:31 +0900

    feat(port): port SolverOperation.php

--- crates/shirabe/src/dependency_resolver/operation/solver_operation.rs
+++ crates/shirabe/src/dependency_resolver/operation/solver_operation.rs
@@ -1,1 +1,11 @@
 //! ref: composer/src/Composer/DependencyResolver/Operation/SolverOperation.php
+
+use crate::dependency_resolver::operation::operation_interface::OperationInterface;
+
+pub trait SolverOperation: OperationInterface {
+    const TYPE: &'static str;
+
+    fn get_operation_type(&self) -> &str {
+        Self::TYPE
+    }
+}
