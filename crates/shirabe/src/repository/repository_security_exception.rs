0813c22eef86c625b5dabae54af14edd2fa73822
nsfisis <nsfisis@gmail.com>
2026-05-12 02:36:57 +0900

    feat(port): port RepositorySecurityException.php

--- crates/shirabe/src/repository/repository_security_exception.rs
+++ crates/shirabe/src/repository/repository_security_exception.rs
@@ -1,1 +1,7 @@
 //! ref: composer/src/Composer/Repository/RepositorySecurityException.php
+
+use shirabe_php_shim::Exception;
+
+/// Thrown when a security problem, like a broken or missing signature
+#[derive(Debug)]
+pub struct RepositorySecurityException(pub Exception);
