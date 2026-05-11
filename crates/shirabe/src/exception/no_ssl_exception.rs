0da5618b197cecf3270f3fbbfd7f0fa371030254
nsfisis <nsfisis@gmail.com>
2026-05-12 02:36:20 +0900

    feat(port): port NoSslException.php

--- crates/shirabe/src/exception/no_ssl_exception.rs
+++ crates/shirabe/src/exception/no_ssl_exception.rs
@@ -1,1 +1,7 @@
 //! ref: composer/src/Composer/Exception/NoSslException.php
+
+use shirabe_php_shim::RuntimeException;
+
+/// Specific exception for Composer\Util\HttpDownloader creation.
+#[derive(Debug)]
+pub struct NoSslException(pub RuntimeException);
