3f334d35e62dc9b1cbb06a71781a942ab39c64d4
nsfisis <nsfisis@gmail.com>
2026-05-12 03:34:20 +0900

    feat(port): port OperationInterface.php

--- crates/shirabe/src/dependency_resolver/operation/operation_interface.rs
+++ crates/shirabe/src/dependency_resolver/operation/operation_interface.rs
@@ -1,1 +1,9 @@
 //! ref: composer/src/Composer/DependencyResolver/Operation/OperationInterface.php
+
+pub trait OperationInterface {
+    fn get_operation_type(&self) -> String;
+
+    fn show(&self, lock: bool) -> String;
+
+    fn to_string(&self) -> String;
+}
