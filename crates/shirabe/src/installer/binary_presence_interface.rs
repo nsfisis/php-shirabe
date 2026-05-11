38c86ad0e6e6862c68f278d922a9c1d4afba737c
nsfisis <nsfisis@gmail.com>
2026-05-12 03:07:01 +0900

    feat(port): port BinaryPresenceInterface.php

--- crates/shirabe/src/installer/binary_presence_interface.rs
+++ crates/shirabe/src/installer/binary_presence_interface.rs
@@ -1,1 +1,7 @@
 //! ref: composer/src/Composer/Installer/BinaryPresenceInterface.php
+
+use crate::package::package_interface::PackageInterface;
+
+pub trait BinaryPresenceInterface {
+    fn ensure_binaries_presence(&self, package: &dyn PackageInterface);
+}
