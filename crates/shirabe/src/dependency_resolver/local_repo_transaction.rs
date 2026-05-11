2734853d7b06b6f432e392dfebac2e5c7f90d6ef
nsfisis <nsfisis@gmail.com>
2026-05-12 03:01:30 +0900

    feat(port): port LocalRepoTransaction.php

--- crates/shirabe/src/dependency_resolver/local_repo_transaction.rs
+++ crates/shirabe/src/dependency_resolver/local_repo_transaction.rs
@@ -1,1 +1,21 @@
 //! ref: composer/src/Composer/DependencyResolver/LocalRepoTransaction.php
+
+use crate::repository::installed_repository_interface::InstalledRepositoryInterface;
+use crate::repository::repository_interface::RepositoryInterface;
+use super::transaction::Transaction;
+
+#[derive(Debug)]
+pub struct LocalRepoTransaction {
+    inner: Transaction,
+}
+
+impl LocalRepoTransaction {
+    pub fn new(locked_repository: &dyn RepositoryInterface, local_repository: &dyn InstalledRepositoryInterface) -> Self {
+        Self {
+            inner: Transaction::new(
+                local_repository.get_packages(),
+                locked_repository.get_packages(),
+            ),
+        }
+    }
+}
