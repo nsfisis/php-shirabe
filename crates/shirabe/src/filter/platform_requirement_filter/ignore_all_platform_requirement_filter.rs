bed53d87c1b29daea7037b1bf7242aae1ffee5b7
nsfisis <nsfisis@gmail.com>
2026-05-12 02:56:23 +0900

    feat(port): port IgnoreAllPlatformRequirementFilter.php

--- crates/shirabe/src/filter/platform_requirement_filter/ignore_all_platform_requirement_filter.rs
+++ crates/shirabe/src/filter/platform_requirement_filter/ignore_all_platform_requirement_filter.rs
@@ -1,1 +1,17 @@
 //! ref: composer/src/Composer/Filter/PlatformRequirementFilter/IgnoreAllPlatformRequirementFilter.php
+
+use crate::filter::platform_requirement_filter::platform_requirement_filter_interface::PlatformRequirementFilterInterface;
+use crate::repository::platform_repository::PlatformRepository;
+
+#[derive(Debug)]
+pub struct IgnoreAllPlatformRequirementFilter;
+
+impl PlatformRequirementFilterInterface for IgnoreAllPlatformRequirementFilter {
+    fn is_ignored(&self, req: &str) -> bool {
+        PlatformRepository::is_platform_package(req)
+    }
+
+    fn is_upper_bound_ignored(&self, req: &str) -> bool {
+        self.is_ignored(req)
+    }
+}
--- crates/shirabe/src/repository/platform_repository.rs
+++ crates/shirabe/src/repository/platform_repository.rs
@@ -1,1 +1,9 @@
 //! ref: composer/src/Composer/Repository/PlatformRepository.php
+
+pub struct PlatformRepository;
+
+impl PlatformRepository {
+    pub fn is_platform_package(req: &str) -> bool {
+        todo!()
+    }
+}
