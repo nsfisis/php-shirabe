d8efe21dbf584edf863aab341cb20437a5675589
nsfisis <nsfisis@gmail.com>
2026-05-12 02:54:36 +0900

    feat(port): port SolverBugException.php

--- crates/shirabe/src/dependency_resolver/solver_bug_exception.rs
+++ crates/shirabe/src/dependency_resolver/solver_bug_exception.rs
@@ -1,1 +1,20 @@
 //! ref: composer/src/Composer/DependencyResolver/SolverBugException.php
+
+use shirabe_php_shim::RuntimeException;
+
+#[derive(Debug)]
+pub struct SolverBugException(pub RuntimeException);
+
+impl SolverBugException {
+    pub fn new(message: String) -> Self {
+        let full_message = format!(
+            "{}\nThis exception was most likely caused by a bug in Composer.\n\
+            Please report the command you ran, the exact error you received, and your composer.json on https://github.com/composer/composer/issues - thank you!\n",
+            message
+        );
+        SolverBugException(RuntimeException {
+            message: full_message,
+            code: 0,
+        })
+    }
+}
