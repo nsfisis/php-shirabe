2715b33a1ddc859a34c0ab8d730a7f1244a1e2ac
nsfisis <nsfisis@gmail.com>
2026-05-12 02:35:34 +0900

    feat(port): port ScriptExecutionException.php

--- crates/shirabe/src/event_dispatcher/script_execution_exception.rs
+++ crates/shirabe/src/event_dispatcher/script_execution_exception.rs
@@ -1,1 +1,7 @@
 //! ref: composer/src/Composer/EventDispatcher/ScriptExecutionException.php
+
+use shirabe_php_shim::RuntimeException;
+
+/// Thrown when a script running an external process exits with a non-0 status code
+#[derive(Debug)]
+pub struct ScriptExecutionException(pub RuntimeException);
