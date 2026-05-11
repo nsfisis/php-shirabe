6c480da44333a90a09b94243aa86c0eb4613031e
nsfisis <nsfisis@gmail.com>
2026-05-12 03:11:59 +0900

    feat(port): port InstalledRepositoryInterface.php

--- crates/shirabe/src/repository/installed_repository_interface.rs
+++ crates/shirabe/src/repository/installed_repository_interface.rs
@@ -1,1 +1,9 @@
 //! ref: composer/src/Composer/Repository/InstalledRepositoryInterface.php
+
+use crate::repository::writable_repository_interface::WritableRepositoryInterface;
+
+pub trait InstalledRepositoryInterface: WritableRepositoryInterface {
+    fn get_dev_mode(&self) -> Option<bool>;
+
+    fn is_fresh(&self) -> bool;
+}
