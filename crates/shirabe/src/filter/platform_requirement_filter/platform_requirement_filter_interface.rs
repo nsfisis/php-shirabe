16fcb72191dea250294ec27ea72f5653cfe4b075
nsfisis <nsfisis@gmail.com>
2026-05-12 02:11:08 +0900

    feat(port): port PlatformRequirementFilterInterface.php

--- crates/shirabe/src/filter/platform_requirement_filter/platform_requirement_filter_interface.rs
+++ crates/shirabe/src/filter/platform_requirement_filter/platform_requirement_filter_interface.rs
@@ -1,1 +1,7 @@
 //! ref: composer/src/Composer/Filter/PlatformRequirementFilter/PlatformRequirementFilterInterface.php
+
+pub trait PlatformRequirementFilterInterface {
+    fn is_ignored(&self, req: &str) -> bool;
+
+    fn is_upper_bound_ignored(&self, req: &str) -> bool;
+}
