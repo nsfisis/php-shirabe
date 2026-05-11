37da0a85a208eada01f61a5c28c945083799f97c
nsfisis <nsfisis@gmail.com>
2026-05-12 04:15:38 +0900

    feat(port): port PreCommandRunEvent.php

--- crates/shirabe/src/plugin/pre_command_run_event.rs
+++ crates/shirabe/src/plugin/pre_command_run_event.rs
@@ -1,1 +1,30 @@
 //! ref: composer/src/Composer/Plugin/PreCommandRunEvent.php
+
+// TODO(plugin): this event is part of the plugin API and is dispatched before a command runs
+use shirabe_external_packages::symfony::console::input::input_interface::InputInterface;
+use crate::event_dispatcher::event::Event;
+
+pub struct PreCommandRunEvent {
+    inner: Event,
+    input: Box<dyn InputInterface>,
+    command: String,
+}
+
+impl PreCommandRunEvent {
+    pub fn new(name: String, input: Box<dyn InputInterface>, command: String) -> Self {
+        let inner = Event::new(name);
+        Self {
+            inner,
+            input,
+            command,
+        }
+    }
+
+    pub fn get_input(&self) -> &dyn InputInterface {
+        self.input.as_ref()
+    }
+
+    pub fn get_command(&self) -> &str {
+        &self.command
+    }
+}
