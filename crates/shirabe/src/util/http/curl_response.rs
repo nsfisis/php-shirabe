8bf5275831a38880c8e85cca6d7b1e3fe5e23991
nsfisis <nsfisis@gmail.com>
2026-05-12 03:36:17 +0900

    feat(port): port CurlResponse.php

--- crates/shirabe/src/util/http/curl_response.rs
+++ crates/shirabe/src/util/http/curl_response.rs
@@ -1,1 +1,31 @@
 //! ref: composer/src/Composer/Util/Http/CurlResponse.php
+
+use indexmap::IndexMap;
+use shirabe_php_shim::PhpMixed;
+
+use super::response::Response;
+
+#[derive(Debug)]
+pub struct CurlResponse {
+    inner: Response,
+    curl_info: IndexMap<String, PhpMixed>,
+}
+
+impl CurlResponse {
+    pub fn new(
+        request: IndexMap<String, PhpMixed>,
+        code: Option<i64>,
+        headers: Vec<String>,
+        body: Option<String>,
+        curl_info: IndexMap<String, PhpMixed>,
+    ) -> Self {
+        Self {
+            inner: Response::new(request, code, headers, body),
+            curl_info,
+        }
+    }
+
+    pub fn get_curl_info(&self) -> &IndexMap<String, PhpMixed> {
+        &self.curl_info
+    }
+}
