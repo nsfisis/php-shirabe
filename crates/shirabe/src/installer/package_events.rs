bcaca91567fdd7845c232fcb3fba301a65bd1b14
nsfisis <nsfisis@gmail.com>
2026-05-14 19:46:43 +0900

    feat(port): port PackageEvents.php

--- crates/shirabe/src/installer/package_events.rs
+++ crates/shirabe/src/installer/package_events.rs
@@ -1,1 +1,12 @@
 //! ref: composer/src/Composer/Installer/PackageEvents.php
+
+pub struct PackageEvents;
+
+impl PackageEvents {
+    pub const PRE_PACKAGE_INSTALL: &'static str = "pre-package-install";
+    pub const POST_PACKAGE_INSTALL: &'static str = "post-package-install";
+    pub const PRE_PACKAGE_UPDATE: &'static str = "pre-package-update";
+    pub const POST_PACKAGE_UPDATE: &'static str = "post-package-update";
+    pub const PRE_PACKAGE_UNINSTALL: &'static str = "pre-package-uninstall";
+    pub const POST_PACKAGE_UNINSTALL: &'static str = "post-package-uninstall";
+}
