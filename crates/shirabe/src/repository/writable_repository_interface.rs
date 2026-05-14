3f3ac8d4ba5efc673efe5f7f686acdfa5970ba4a
nsfisis <nsfisis@gmail.com>
2026-05-14 19:46:17 +0900

    feat(port): port WritableRepositoryInterface.php

--- crates/shirabe/src/repository/writable_repository_interface.rs
+++ crates/shirabe/src/repository/writable_repository_interface.rs
@@ -1,1 +1,22 @@
 //! ref: composer/src/Composer/Repository/WritableRepositoryInterface.php
+
+use anyhow::Result;
+use crate::installer::installation_manager::InstallationManager;
+use crate::package::package_interface::PackageInterface;
+use crate::repository::repository_interface::RepositoryInterface;
+
+pub trait WritableRepositoryInterface: RepositoryInterface {
+    fn write(&mut self, dev_mode: bool, installation_manager: &InstallationManager) -> Result<()>;
+
+    fn add_package(&mut self, package: Box<dyn PackageInterface>) -> Result<()>;
+
+    fn remove_package(&mut self, package: &dyn PackageInterface) -> Result<()>;
+
+    fn get_canonical_packages(&self) -> Vec<Box<dyn PackageInterface>>;
+
+    fn reload(&mut self);
+
+    fn set_dev_package_names(&mut self, dev_package_names: Vec<String>);
+
+    fn get_dev_package_names(&self) -> &Vec<String>;
+}
