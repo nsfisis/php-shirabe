72288497e8a4cac3a3696d863de6a803bc85d773
nsfisis <nsfisis@gmail.com>
2026-05-14 19:53:36 +0900

    feat(port): port PluginEvents.php

--- crates/shirabe/src/plugin/plugin_events.rs
+++ crates/shirabe/src/plugin/plugin_events.rs
@@ -1,1 +1,12 @@
 //! ref: composer/src/Composer/Plugin/PluginEvents.php
+
+pub struct PluginEvents;
+
+impl PluginEvents {
+    pub const INIT: &'static str = "init";
+    pub const COMMAND: &'static str = "command";
+    pub const PRE_FILE_DOWNLOAD: &'static str = "pre-file-download";
+    pub const POST_FILE_DOWNLOAD: &'static str = "post-file-download";
+    pub const PRE_COMMAND_RUN: &'static str = "pre-command-run";
+    pub const PRE_POOL_CREATE: &'static str = "pre-pool-create";
+}
