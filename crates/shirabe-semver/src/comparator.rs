ef1bdb737d73125ab71dd9ff5f0f135ce7aaba07
nsfisis <nsfisis@gmail.com>
2026-05-16 23:34:08 +0900

    feat(port): port Comparator.php

--- crates/shirabe-semver/src/comparator.rs
+++ crates/shirabe-semver/src/comparator.rs
@@ -1,1 +1,36 @@
 //! ref: composer/vendor/composer/semver/src/Comparator.php
+
+use crate::constraint::constraint::Constraint;
+
+pub struct Comparator;
+
+impl Comparator {
+    pub fn greater_than(version1: String, version2: String) -> bool {
+        Self::compare(version1, ">".to_string(), version2)
+    }
+
+    pub fn greater_than_or_equal_to(version1: String, version2: String) -> bool {
+        Self::compare(version1, ">=".to_string(), version2)
+    }
+
+    pub fn less_than(version1: String, version2: String) -> bool {
+        Self::compare(version1, "<".to_string(), version2)
+    }
+
+    pub fn less_than_or_equal_to(version1: String, version2: String) -> bool {
+        Self::compare(version1, "<=".to_string(), version2)
+    }
+
+    pub fn equal_to(version1: String, version2: String) -> bool {
+        Self::compare(version1, "==".to_string(), version2)
+    }
+
+    pub fn not_equal_to(version1: String, version2: String) -> bool {
+        Self::compare(version1, "!=".to_string(), version2)
+    }
+
+    pub fn compare(version1: String, operator: String, version2: String) -> bool {
+        let constraint = Constraint::new(operator, version2);
+        constraint.match_specific(&Constraint::new("==".to_string(), version1), true)
+    }
+}
