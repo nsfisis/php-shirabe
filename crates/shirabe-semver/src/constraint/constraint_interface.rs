7205727a446cc13274b512a9971f2ab477eff253
nsfisis <nsfisis@gmail.com>
2026-05-16 23:31:24 +0900

    feat(port): port ConstraintInterface.php

--- crates/shirabe-semver/src/constraint/constraint_interface.rs
+++ crates/shirabe-semver/src/constraint/constraint_interface.rs
@@ -1,1 +1,19 @@
 //! ref: composer/vendor/composer/semver/src/Constraint/ConstraintInterface.php
+
+use crate::constraint::bound::Bound;
+
+pub trait ConstraintInterface {
+    fn matches(&self, provider: &dyn ConstraintInterface) -> bool;
+
+    fn compile(&self, other_operator: i64) -> String;
+
+    fn get_upper_bound(&self) -> Bound;
+
+    fn get_lower_bound(&self) -> Bound;
+
+    fn get_pretty_string(&self) -> String;
+
+    fn set_pretty_string(&mut self, pretty_string: Option<String>);
+
+    fn __to_string(&self) -> String;
+}
