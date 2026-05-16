5b3d5a44c86fed94fc9d915943b728903f09c8ca
nsfisis <nsfisis@gmail.com>
2026-05-16 23:31:35 +0900

    feat(port): port MatchAllConstraint.php

--- crates/shirabe-semver/src/constraint/match_all_constraint.rs
+++ crates/shirabe-semver/src/constraint/match_all_constraint.rs
@@ -1,1 +1,44 @@
 //! ref: composer/vendor/composer/semver/src/Constraint/MatchAllConstraint.php
+
+use crate::constraint::bound::Bound;
+use crate::constraint::constraint_interface::ConstraintInterface;
+
+#[derive(Debug)]
+pub struct MatchAllConstraint {
+    pub(crate) pretty_string: Option<String>,
+}
+
+impl ConstraintInterface for MatchAllConstraint {
+    fn matches(&self, _provider: &dyn ConstraintInterface) -> bool {
+        true
+    }
+
+    fn compile(&self, _other_operator: i64) -> String {
+        "true".to_string()
+    }
+
+    fn set_pretty_string(&mut self, pretty_string: Option<String>) {
+        self.pretty_string = pretty_string;
+    }
+
+    fn get_pretty_string(&self) -> String {
+        if let Some(ref s) = self.pretty_string {
+            if !s.is_empty() {
+                return s.clone();
+            }
+        }
+        self.__to_string()
+    }
+
+    fn __to_string(&self) -> String {
+        "*".to_string()
+    }
+
+    fn get_upper_bound(&self) -> Bound {
+        Bound::positive_infinity()
+    }
+
+    fn get_lower_bound(&self) -> Bound {
+        Bound::zero()
+    }
+}
